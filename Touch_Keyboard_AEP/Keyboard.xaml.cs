97961928e2f019f873de52c7a6034f919f25b8d4
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-03-23 17:06:30 +0100

    initial commit

---
+++ Touch_Keyboard_AEP/Keyboard.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Touch_Keyboard_AEP
+{
+    public partial class Keyboard : UserControl
+    {
+        public EventHandler<string> KeyPressedEvent = (s, e) => { };
+        private bool isUppercase = false;
+
+        public Keyboard()
+        {
+            InitializeComponent();
+            InitializeKeypressEvents();
+        }
+
+        private void InitializeKeypressEvents()
+        {
+            foreach (var child in gRow1.Children)
+            {
+                var key = child as KeyboardKey;
+                key.KeyUp += (s, e) => { Keypressed(e); };
+            }
+            foreach (var child in gRow2.Children)
+            {
+                var key = child as KeyboardKey;
+                key.KeyUp += (s, e) => { Keypressed(e); };
+            }
+            foreach (var child in gRow3.Children)
+            {
+                var key = child as KeyboardKey;
+                key.KeyUp += (s, e) => { Keypressed(e); };
+            }
+            foreach (var child in gRow4.Children)
+            {
+                var key = child as KeyboardKey;
+
+                if (key.KeyText != "MAIUSC")
+                    key.KeyUp += (s, e) => { Keypressed(e); };
+            }
+            foreach (var child in gRow5.Children)
+            {
+                var key = child as KeyboardKey;
+                key.KeyUp += (s, e) => { Keypressed(e); };
+            }
+        }
+        private void Keypressed(string key)
+        {
+            KeyPressedEvent(this, key);
+        }
+
+        private void SwitchUppercase()
+        {
+            if (!isUppercase)
+            {
+                foreach (var child in gRow1.Children)
+                {
+                    var key = child as KeyboardKey;
+                    key.KeyText = key.KeyText.ToUpper();
+                }
+                foreach (var child in gRow2.Children)
+                {
+                    var key = child as KeyboardKey;
+                    key.KeyText = key.KeyText.ToUpper();
+                }
+                foreach (var child in gRow3.Children)
+                {
+                    var key = child as KeyboardKey;
+                    key.KeyText = key.KeyText.ToUpper();
+                }
+                foreach (var child in gRow4.Children)
+                {
+                    var key = child as KeyboardKey;
+
+                    if (key.KeyText != "MAIUSC")
+                        key.KeyText = key.KeyText.ToUpper();
+                }
+                isUppercase = true;
+            }
+            else
+            {
+                foreach (var child in gRow1.Children)
+                {
+                    var key = child as KeyboardKey;
+                    key.KeyText = key.KeyText.ToLower();
+                }
+                foreach (var child in gRow2.Children)
+                {
+                    var key = child as KeyboardKey;
+                    key.KeyText = key.KeyText.ToLower();
+                }
+                foreach (var child in gRow3.Children)
+                {
+                    var key = child as KeyboardKey;
+                    key.KeyText = key.KeyText.ToLower();
+                }
+                foreach (var child in gRow4.Children)
+                {
+                    var key = child as KeyboardKey;
+
+                    if (key.KeyText != "MAIUSC")
+                        key.KeyText = key.KeyText.ToLower();
+                }
+                isUppercase = false;
+            }
+        }
+
+        private void Uppercase_Down(object sender, MouseButtonEventArgs e)
+        {
+            SwitchUppercase();
+        }
+    }
+}
---
+++ Touch_Keyboard_AEP/KeyboardKey.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Touch_Keyboard_AEP
+{
+    public partial class KeyboardKey : UserControl
+    {
+        public EventHandler<string> KeyDown = (s, e) => { };
+        public EventHandler<string> KeyUp = (s, e) => { };
+
+        SolidColorBrush background = new SolidColorBrush(Colors.DarkGray);
+        SolidColorBrush backgroundPressed = new SolidColorBrush(Colors.Gray);
+
+        public string KeyText
+        {
+            get { return (string)GetValue(KeyTextProperty); }
+            set { SetValue(KeyTextProperty, value); }
+        }
+        public static readonly DependencyProperty KeyTextProperty =
+            DependencyProperty.Register("KeyText", typeof(string), typeof(KeyboardKey), new PropertyMetadata("x"));
+
+        public KeyboardKey()
+        {
+            InitializeComponent();
+        }
+        private void ButtonPressed()
+        {
+            border.Background = backgroundPressed;
+            KeyDown(this, KeyText);
+        }
+        private void ButtonReleased()
+        {
+            border.Background = background;
+            KeyUp(this, KeyText);
+        }
+
+        private void key_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ButtonPressed();
+        }
+
+        private void key_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ButtonReleased();
+        }
+
+        private void key_MouseLeave(object sender, MouseEventArgs e)
+        {
+            border.Background = background;
+        }
+    }
+}
